883adf6169411bd2be9ef1685d0e3a8a5628fc25
inciyrdm <95236590+inciyrdm@users.noreply.github.com>
2022-01-23 16:02:48 +0300

    Add files via upload

---
+++ nesne_final.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 23 Oca 2022, 13:20:58
+-- Sunucu sürümü: 10.4.17-MariaDB
+-- PHP Sürümü: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `nesne`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `nesne final`
+--
+-- nesne.nesne final tablosu için yapı okuma hatası: #1146 - Table 'nesne.nesne final' doesn't exist
+-- nesne.nesne final tablosu için veri okuma hatası: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `nesne`.`nesne final`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
